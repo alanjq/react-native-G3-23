19cd694b695ad5e7ac9f80250d38946a8faeb518
Alan Jimenez <alanjq@outlook.com>
2023-11-01 23:18:18 -0500

    Archivos sesión 2

---
+++ sesion-2/depuracion-expo/components/SideScreen.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+
+export default function SideScreen() {
+    return (
+        <View>
+            <Text>Otra pantalla, pero si estilos</Text>
+        </View>
+    );
+}
---
+++ sesion-2/depuracionrn/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import HomeScreen from './components/HomeScreen';
+
+function App(): JSX.Element {
+  return <HomeScreen />
+}
+export default App;
