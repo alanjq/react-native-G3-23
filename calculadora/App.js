4ee99f6a15f8cc106b9f9fae629050cdad5f6cd2
Alan Jimenez <alanjq@outlook.com>
2023-11-27 22:08:35 -0500

    Calculadora funcional

--- calculadora/App.js
+++ calculadora/App.js
@@ -1,52 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Boton from './components/Boton';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function App() {
   const [input, setInput] = useState(0)
-  const [memoria, setMemoria] = useState(0)
+  const [total, setTotal] = useState(null)
+  const [signo, setSigno] = useState(null)
+  const [prevInput, setPrevInput] = useState(0)
   const botones = [1, 2, 3, '+',
     4, 5, 6, '-',
     7, 8, 9, 'x',
     'C', 0, '=', '/']
 
-  const handlePress = (valor) => {
-    if (!isNaN(valor)) {
-      setInput(Number(input + '' + valor))
-      console.log('Es número', valor);
-    } else if (valor === 'C') {
-      setInput(0)
-      setMemoria(0)
-      console.log('Borrar número', valor);
-    } else if (valor === '=') {
-      console.log('Mostrar resultado', valor);
-    } else if (valor === '+' || valor === '-' || valor === '/' || valor === 'x') {
-      console.log('Realizar operación', valor);
-      console.log('Memoria', memoria);
-      console.log('input', input);
-      let resultado = memoria
-      switch (valor) {
-        case '+':
-          resultado = memoria + valor
-          break;
-
-        case '-': // revisar
-          resultado = memoria + valor
-          break;
-
-        case 'x': // Corregir
-          resultado = memoria + valor
-          break;
-
-        case '/': // corregir
-          resultado = memoria + valor
-          break;
+  const handlePress = (tecla) => {
+    // Solo aceptar números
+    if (!isNaN(tecla)) {
+      setInput(Number(input + "" + tecla))
+    } else {
+      let resultado = calcular(signo, prevInput, input)
+      setTotal(resultado)
+      setPrevInput(total)
+      if (tecla === '=') {
+        setSigno(null)
+      } else {
+        setSigno(tecla)
+        // Permite seguir realizando operaciones después de obtener el resultado
+        setPrevInput(total || input)
+        setInput(0)
       }
-      setMemoria(resultado)
-      setInput(0)
-
     }
+  }
+
+  const resetCalculadora = () => {
+    setSigno(null)
+    setInput(0)
+    setTotal(null)
+    setPrevInput(0)
+  }
+
+
+  const calcular = (operacion, a, b) => {
+    switch (operacion) {
+      case "+": return suma(a, b);
+      case "-": return resta(a, b);
+      case "x": return multiplicacion(a, b);
+      case "/": return division(a, b);
+      case "C":
+        resetCalculadora()
+        break;
+    }
+  }
+
+  const suma = (a, b) => a + b
+  const resta = (a, b) => a - b
+  const multiplicacion = (a, b) => a * b
+  const division = (a, b) => {
+    if (a === 0 || b === 0) {
+      return 0
+    }
+    return a / b
   }
 
   return (
@@ -55,7 +68,7 @@ export default function App() {
 
       <View style={styles.contenedor}>
         <View>
-          <TextInput style={styles.textInput} value={input} editable={false} />
+          <TextInput style={styles.textInput} value={total || input} editable={false} />
         </View>
         <View style={styles.fila}>
           {botones.slice(0, 4).map((boton, i) => <Boton key={i} title={boton} onPress={handlePress} style={styles.botonC} />)}
